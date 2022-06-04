8a1d89eee1bfccc08cb85ef67d72cff1552800f9
J1Loop <jorge.ua.1996@gmail.com>
2022-06-04 18:50:20 +0200

    HTML + CSS básico hecho, retoques later. JS template de lista hecho

---
+++ js/lista.js
@@ -0,0 +1,26 @@
+const listaTareas = [
+    {
+        id: 1,
+        name: 'Una tarea.',
+        info: 'Hay que hacer algo... urgente.',
+        priority: 1,
+    },
+    {
+        id: 2,
+        name: 'Otra tarea.',
+        info: 'Hay que hacer algo... diario.',
+        priority: 2,
+    },
+    {
+        id: 3,
+        name: 'Tercera tarea.',
+        info: 'Hay que hacer algo... semanal.',
+        priority: 3,
+    },
+    {
+        id: 4,
+        name: 'Cuarta tarea.',
+        info: 'Hay que hacer algo... mensual.',
+        priority: 4,
+    },
+]
