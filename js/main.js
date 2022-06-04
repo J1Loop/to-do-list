4b12b11995112da157cb71ebef04fdd770914830
J1Loop <jorge.ua.1996@gmail.com>
2022-06-04 20:26:39 +0200

    HTML / CSS - Retoques varios. JS - Pintar tareas de la lista terminado.
    Cierro branch.

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// Capturar elementos del DOM con los que interactuar:
+const searchInput = document.querySelector('#search-input');
+const prioritySelect = document.querySelector('#priority-select');
+const addInput = document.querySelector('#add-input');
+const sectionTarea = document.querySelector('#list-tarea')
+
+// Pintar lista de tareas:
+function printAllTareas(pList, pDom) {
+    pDom.innerHTML = '';
+    pList.forEach(tarea => printOneTarea(tarea, pDom));
+}
+
+function printOneTarea(pTarea, pDom) {
+    let ul = document.createElement('ul');
+    let li = document.createElement('li');
+    let textDiv = document.createElement('div');
+    let h3 = document.createElement('h3');
+    h3.innerText = `${pTarea.name}`
+    let p = document.createElement('p');
+    p.innerText = `${pTarea.info}`
+    let iconosDiv = document.createElement('div');
+    iconosDiv.innerHTML = `<i data-id="${pTarea.id}" class="fa-solid fa-check"></i>
+                     <i data-id="${pTarea.id}" class="fa-solid fa-trash"></i>`
+
+    switch (pTarea.priority) {
+        case 1:
+            li.classList.add('urgente');
+            break;
+        case 2:
+            li.classList.add('diaria');
+            break;
+        case 3:
+            li.classList.add('semanal');
+            break;
+        case 4:
+            li.classList.add('mensual');
+            break;
+    }
+
+    textDiv.append(h3, p);
+    li.append(textDiv, iconosDiv);
+    ul.appendChild(li);
+    pDom.appendChild(ul);
+    console.log(pTarea);
+
+}
+
+printAllTareas(listaTareas, sectionTarea)
