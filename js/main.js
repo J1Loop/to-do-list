3558cf3338aaa53fb5044c3a8f77547e4bf031d9
J1Loop <jorge.ua.1996@gmail.com>
2022-06-04 21:20:20 +0200

    Buscador semántico a medias, faltan filtros. crear branch

--- js/main.js
+++ js/main.js
@@ -6,23 +6,30 @@ const sectionTarea = document.querySelector('#list-tarea')
 
 // Pintar lista de tareas:
 function printAllTareas(pList, pDom) {
+
     pDom.innerHTML = '';
     pList.forEach(tarea => printOneTarea(tarea, pDom));
 }
 
 function printOneTarea(pTarea, pDom) {
+
     let ul = document.createElement('ul');
     let li = document.createElement('li');
     let textDiv = document.createElement('div');
+
     let h3 = document.createElement('h3');
     h3.innerText = `${pTarea.name}`
+
     let p = document.createElement('p');
     p.innerText = `${pTarea.info}`
+
     let iconosDiv = document.createElement('div');
     iconosDiv.innerHTML = `<i data-id="${pTarea.id}" class="fa-solid fa-check"></i>
                      <i data-id="${pTarea.id}" class="fa-solid fa-trash"></i>`
 
+
     switch (pTarea.priority) {
+
         case 1:
             li.classList.add('urgente');
             break;
@@ -49,8 +56,13 @@ printAllTareas(listaTareas, sectionTarea)
 
 // Buscar semánticamente
 
-searchInput.addEventListener('keydown', addTarea);
+searchInput.addEventListener('input', getSearch);
 
-function addTarea(event) {
-    console.log(event.target.value);
+function getSearch(event) {
+    // console.log(event.target.value);
+    let search = event.target.value;
+    printAllTareas(filterByName(listaTareas, search), sectionTarea)
+    
+
+
 }
